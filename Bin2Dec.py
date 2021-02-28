c0064dbca1d6b5a98883334f8b5e2b78de7ce9a8
johnnymundi <johnnymundi@hotmail.com>
2021-02-28 20:00:26 -0300

    first commit - add Bin2Dec.py

---
+++ Bin2Dec.py
@@ -0,0 +1,13 @@
+'''
+allows the user to enter strings of up to 8 binary digits, 0's and 1's, in any sequence and then
+displays its decimal equivalent.
+
+Conditions:
+- Arrays may not be used to contain the binary digits entered by the user;
+- Determining the decimal equivalent of a particular binary digit in the sequence must be calculated using
+a single mathematical function, for example the natural logarithm. It's up to you to figure out which
+function to use.
+
+
+
+'''
